313fbc60789055dda7d6ab5215501c518a86e233
tim geaney <timgeaney24@gmail.com>
2013-08-04 17:53:02 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripPlanner::Application.config.secret_token = 'a1cef4a99bd312a71fb7e0667f25a960a6ab2850658a3804ec8f7e0f028589533cfa5c2404d46f287e385a4e623de40cb9dd7abff09f934bff3aaa0bc1f6643f'
